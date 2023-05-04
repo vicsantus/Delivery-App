737f0ce5aeeb6bc4e9b14187196eee5b7c86d7a8
brunosbrito <brunobrito728@gmail.com>
2023-05-04 17:50:24 -0300

    feat: faz a verificação do login

--- back-end/src/controllers/loginController.js
+++ back-end/src/controllers/loginController.js
@@ -2,10 +2,14 @@ const loginService = require('../services/loginService');
 
 const getByUser = async (req, res) => {
   const { email, password } = req.body;
-
-  const user = await loginService.getByUser(email, password);
-
-  if (!user) return res.status(404).json({ message: 'Not found' });
+  
+  try {
+    const user = await loginService.getByUser(email, password);
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(404).json({ menssagem: 'Password/Email not found' });
+  }
+  // if (!user) return res.status(404).json({ message: 'Not found' });
 };
 
 module.exports = {
--- back-end/src/services/loginService.js
+++ back-end/src/services/loginService.js
@@ -1,22 +1,15 @@
-// const bcrypt = require('bcryptjs');
-// const md5 = require('md5');
-// const crypto = require('crypto');
+const crypto = require('crypto');
 const { User } = require('../database/models/index');
 
-const getByUser = async (email, _password) => {
+const getByUser = async (email, password) => {
   const result = await User.findOne({ where: { email } });
-  // const senhaHash = crypto.createHash('md5').update(password).digest('hex');
+  const senhaHash = crypto.createHash('md5').update(password).digest('hex');
 
-  console.log(result);
-  // console.log(result.password, password);
-  // if (senhaHash === result.password) {
-  //   console.log('-----------------A senha está correta!');
-  // } else {
-  //   console.log('-----------------A senha está incorreta.');
-  // }
-  // if (!result || !bcrypt.compareSync(password, result.password)) {
-  //   throw new Error('Password/Email not found');
-  // }
+  const { password: _, ...user } = result.dataValues; 
+  if (senhaHash === result.password) {
+    return user;
+  } 
+    throw new Error('Password/Email not found');
 };
 
 module.exports = {
