6aa2f83222a13d0c3da07ed9ed7138c2d5af820f
Cozmu <www.jorgereis@gmail.com>
2023-05-08 15:08:10 -0300

    refactor: testo dinamico no nav bar

---
+++ front-end/src/pages/Seler.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+
+export default function Seler() {
+  return (
+    <>
+      <NavBar />
+      <div>Seler</div>
+    </>
+  );
+}
+
+// fulana@deliveryapp.com
+// fulana@123
