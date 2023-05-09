dcc0062e78b64a25989e15b1fc86deb8807961ab
victrosantos <victor.santos.fk@hotmail.com>
2023-05-09 17:59:39 -0300

    fix: corrigido getall de sales para retornar todos os produtos da sale em uma lista e add seed de salesproducts

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
-import './App.css';
 import { Route, Switch, useHistory, useLocation } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import './App.css';
 import Client from './pages/Client';
+import Login from './pages/Login';
+import OrderDetails from './pages/OrderDetails';
+import Register from './pages/Register';
 import Seler from './pages/Seler';
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
       />
       <Route exact path="/register" component={ Register } />
       <Route exact path="/login" component={ Login } />
+      <Route exact path="/seller/orders/:id_do_produto" component={ OrderDetails } />
       <Route path="/seller/orders" component={ Seler } />
       <Route path="/client" component={ Client } />
     </Switch>
