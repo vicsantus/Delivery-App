98352e2064f15da1755252c3134e67802500d4df
victrosantos <victor.santos.fk@hotmail.com>
2023-05-10 13:32:14 -0300

    fix: Corrigido retorno do put sales e terminado orderDetails

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { 
-adminRuter, 
-imageRouter, 
-loginRouter, 
-productsRouter, 
-registerRouter, 
-salesRouter,
-} = require('../routes');
-const errorMiddleware = require('../middleware/ErrorMiddleware');
+const router = require('../routes');
 
 const app = express();
 
@@ -22,12 +14,6 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.get('/coffee', (_req, res) => res.status(418).end());
-app.use('/admin', adminRuter);
-app.use('/images', imageRouter);
-app.use('/login', loginRouter);
-app.use('/products', productsRouter);
-app.use('/register', registerRouter);
-app.use('/sales', salesRouter);
-app.use(errorMiddleware);
+app.use(router);
 
 module.exports = app;
--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,15 +1,20 @@
+const express = require('express');
 const adminRuter = require('./admin');
 const imageRouter = require('./image');
 const loginRouter = require('./login');
 const productsRouter = require('./products');
 const registerRouter = require('./register');
 const salesRouter = require('./sales');
+const errorMiddleware = require('../middleware/ErrorMiddleware');
 
-module.exports = {
-  adminRuter,
-  imageRouter,
-  loginRouter,
-  productsRouter,
-  registerRouter,
-  salesRouter,
-};
+const router = express();
+
+router.use('/admin', adminRuter);
+router.use('/images', imageRouter);
+router.use('/login', loginRouter);
+router.use('/products', productsRouter);
+router.use('/register', registerRouter);
+router.use('/sales', salesRouter);
+router.use(errorMiddleware);
+
+module.exports = router;
