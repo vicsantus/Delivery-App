a6998398a1ec40c61732b63469d56853b27abff0
Beatriz <beatriz.mediatto2@gmail.com>
2023-05-10 17:43:05 -0300

    feat: implementa table

---
+++ front-end/src/components/Table.js
@@ -0,0 +1,57 @@
+import PropTypes from 'prop-types';
+
+export default function Table({ users }) {
+  return (
+    <table>
+      <thead>
+        <th>Item</th>
+        <th>Nome</th>
+        <th>E-mail</th>
+        <th>Tipo</th>
+        <th>Excluir</th>
+      </thead>
+      <tbody>
+        {users?.map((person, index) => (
+          <tr key={ index }>
+            <td
+              data-testid={ `admin_manage__element-user-table-item-number${index}` }
+            >
+              {person.id}
+
+            </td>
+            <td
+              data-testid={ `admin_manage__element-user-table-name-${index}` }
+            >
+              {person.name}
+
+            </td>
+            <td
+              data-testid={ `admin_manage__element-user-table-email-${index}` }
+            >
+              {person.email}
+
+            </td>
+            <td
+              data-testid={ `admin_manage__element-user-table-role-${index}` }
+            >
+              {person.role}
+
+            </td>
+            <td>
+              <button
+                type="button"
+                data-testid={ `admin_manage__element-user-table-remove-${index}` }
+              >
+                Excluir
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+Table.propTypes = {
+  users: PropTypes.array,
+}.isRequired;
--- front-end/src/pages/Admin.js
+++ front-end/src/pages/Admin.js
@@ -1,7 +1,11 @@
 import AdminForm from '../components/AdminForm';
+import Table from '../components/Table';
 
 export default function Admin() {
   return (
-    <AdminForm />
+    <div>
+      <AdminForm />
+      <Table users={ users } />
+    </div>
   );
 }
